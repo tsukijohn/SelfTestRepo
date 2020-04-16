4ef50efada6a30856bb41ce5050783a88cba3465
admin <admin@gitee.com>
2020-04-16 15:52:51 +0800

    Commit

---
+++ src/ml/mikami/Test.java
@@ -0,0 +1,8 @@
+package ml.mikami;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test Conflict");
+        System.out.println("Test Conflict");
+    }
+}
---
+++ src/ml/mikami/Test2.java
@@ -0,0 +1,10 @@
+package ml.mikami;
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("Test Conflict Conflict Test");
+        System.out.println("Test Conflict");
+        System.out.println("Test Conflict");
+        System.out.println("Test Conflict");
+    }
+}
